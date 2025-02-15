a7ec452f67f8aade2e5aad83a56eebfe0c98a3ac
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-02-15 10:38:02 +0100

    Add files via upload

---
+++ style-guide.md
@@ -0,0 +1,16 @@
+### Primary
+
+- Orange: hsl(26, 100%, 55%)
+- Pale orange: hsl(25, 100%, 94%)
+- Light Red (input::focus): hsl(0, 100%, 63%) 
+
+### Neutral
+- Very Dark Cyan: hsl(192, 100%, 9%)
+
+### Fonts
+- Oswald 
+- Merriweather
+
+### Icons
+- Font awesome
+
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    colors: {
+      pink: 'hsl(322, 100%, 66%)',
+      lightpink: 'hsl(321, 100%, 78%)',
+      lightred: 'hsl(0, 100%, 63%)',
+      verydarkcyan: 'hsl(192, 100%, 9%)'
+      // Add more colors as needed
+    },
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
